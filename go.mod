d718b8d7e164c826488e1598ff257f0a581569b7
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2023-01-09 09:02:15 +0900

    param get for env

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/kaepa3/mecaber
 go 1.19
 
 require github.com/bluele/mecab-golang v0.0.0-20180831023624-c8cfe04e87f9
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- mecmd/main.go
+++ mecmd/main.go
@@ -3,24 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/joho/godotenv"
 	"github.com/kaepa3/mecaber"
 )
 
+const (
+	confFile = ".mecaber"
+)
+
+func readConfig() {
+	paths := []string{
+		filepath.Join(".env"),
+		filepath.Join(os.Getenv("HOME"), confFile),
+		filepath.Join(os.Getenv("HOMEPATH"), confFile),
+	}
+	for _, v := range paths {
+		if err := godotenv.Load(v); err == nil {
+			return
+		}
+		log.Println(v)
+	}
+	log.Println("Error loading .env file", paths)
+	return
+}
+
+func getDic() string {
+	readConfig()
+	dic := os.Getenv("DICTIONARY")
+	if len(dic) == 0 {
+		return ""
+	}
+	return "-d " + dic
+}
+
 func main() {
 	flag.Parse()
-	args := flag.Args()
-	if len(args) < 1 {
-		panic("args error")
-	}
-	err, m := mecaber.CreateNew()
+	err, m := mecaber.CreateNew(getDic())
 	if err != nil {
 		panic(err)
 	}
 	defer m.Destroy()
 
 	done := make(chan struct{})
-	errCh, node := m.ParseToNode(args[0], done)
+	errCh, node := m.ParseToNode(flag.Args()[0], done)
 	if err != nil {
 		panic(err)
 	}
